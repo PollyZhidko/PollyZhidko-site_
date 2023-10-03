b88e48bd2f245551f7b538c5ba3ade3f647f032a
PollyZhidko <144808280+PollyZhidko@users.noreply.github.com>
2023-10-03 09:55:46 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     const initialInvestmentInput = document.getElementById("initial-investment");
-    const currentPriceInput = document.getElementById("current-price");
-    const quantityInput = document.getElementById("quantity");
+    const currentValueInput = document.getElementById("current-value");
+    const profitInput = document.getElementById("profit");
     const calculateButton = document.getElementById("calculate-button");
-    const profitResult = document.getElementById("profit-result");
 
     calculateButton.addEventListener("click", function () {
         const initialInvestment = parseFloat(initialInvestmentInput.value);
-        const currentPrice = parseFloat(currentPriceInput.value);
-        const quantity = parseFloat(quantityInput.value);
+        const currentValue = parseFloat(currentValueInput.value);
 
-        if (isNaN(initialInvestment) || isNaN(currentPrice) || isNaN(quantity)) {
-            profitResult.textContent = "Invalid input";
-            return;
+        if (!isNaN(initialInvestment) && !isNaN(currentValue)) {
+            const profit = currentValue - initialInvestment;
+            profitInput.value = profit.toFixed(2);
+        } else {
+            profitInput.value = "Invalid input";
         }
-
-        const profit = (currentPrice * quantity) - initialInvestment;
-        profitResult.textContent = `$${profit.toFixed(2)}`;
     });
 });
 
